931a601ac3c3c617cbc8b75f8eda8fc78d821420
Abdallah <abdallah.halees@gmail.com>
2017-10-01 00:44:13 +0300

    controller to create a profile

---
+++ src/controllers/createProfile.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const formidable = require('formidable');
+const request = require('request');
+const nconf = require('nconf').file({ 'file': 'config.json' }).env();
+const oxfordList = 'test_face_list';
+const oxfordKey = nconf.get('OXFORD_SECRET_KEY');
+module.exports = (req, res, next) => {
+  var Person = mongoose.model('Person');
+
+  var form = new formidable.IncomingForm();
+
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      console.log(err);
+    } else {
+      var person = new Person(fields);
+      person.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(fields.face_id);
+          request.post({
+            'url': 'https://eastasia.api.cognitive.microsoft.com/face/v1.0/facelists/' + oxfordList + '/persistedFaces',
+            'headers': {
+              'Content-Type': 'application/json',
+              'Ocp-Apim-Subscription-Key': oxfordKey
+            },
+            'body': JSON.stringify({
+              'url': fields.face_id
+            })
+          },
+              function (error, response, body) {
+                console.log('response', response.statusCode);
+                console.log('body', body);
+                body = JSON.parse(body);
+                if (body.error) {
+                  console.log(body.error);
+                } else {
+                  var model = mongoose.model('Person');
+                  model.update({ '_id': person._id }, {$set: { face_id: body.persistedFaceId }}, function (err, user) {
+                    if (err) {
+                      console.log(err);
+                      res.write('Please try again.');
+                    }
+
+                    res.send('Success');
+                  });
+                }
+              });
+        }
+      });
+    }
+  });
+};
