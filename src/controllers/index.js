f99c6ef303ef2aeeb6d4d4de2fc922a716d7ebd0
Abdallah <abdallah.halees@gmail.com>
2017-10-01 00:44:51 +0300

    add routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const createProfile = require('./createProfile.js');
+const verifyFace = require('./verifyFace');
 
 const router = express.Router();
+
+router.post('/create', createProfile);
+router.post('/verify', verifyFace);
 
 module.exports = router;
